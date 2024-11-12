a64a508897a5137e568def475370259eacea2bc6
Beam <jkpbeam@gmail.com>
2024-11-12 22:46:32 +0700

    12 nov

--- server/server.mjs
+++ server/server.mjs
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import postRouter from "./routers/post.mjs";
 import getRouter from "./routers/get.mjs";
-import putRouter from "./routers/put.mjs"
+import putRouter from "./routers/put.mjs";
 import deleteRouter from "./routers/delete.mjs";
 
 const app = express();
@@ -21,6 +21,7 @@ app.use(postRouter);
 app.use(putRouter);
 app.use(deleteRouter);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
